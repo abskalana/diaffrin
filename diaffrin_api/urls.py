6f5c451511f652ad703dca3f0c34e7a482d3b1c9
absidibe <gouandiaka@gmail.com>
2024-03-15 14:51:18 +0100

    fix url

--- diaffrin_api/urls.py
+++ diaffrin_api/urls.py
@@ -9,7 +9,7 @@ from django.views.static import serve
 favicon_view = RedirectView.as_view(url='/static/images/favicon.ico', permanent=True)
 
 urlpatterns = [
-    path("home", views.home, name="home"),
+    path("", views.home, name="home"),
     path('entity/<slug:slug>/', views.get_entity, name="entity_detail"),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
