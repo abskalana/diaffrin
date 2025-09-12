2b69948867f7305ebc437985d3b37b50e10b3770
absidibe <gouandiaka@gmail.com>
2025-09-12 09:29:08 +0000

    hello

--- diaffrin_api/views.py
+++ diaffrin_api/views.py
@@ -34,7 +34,7 @@ def login_view(request):
             username = request.POST.get("username")
             password = request.POST.get("password")
             employer = get_object_or_404(Personnel, id=username)
-            if employer.password == password
+            if employer.password == password:
                 return HttpResponse("true", content_type="text/plain")
     return  HttpResponse("false", content_type="text/plain")
 
