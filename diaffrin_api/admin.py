395e8fe73e1a680e14c2bd635745d41f7398fe4f
absidibe <gouandiaka@gmail.com>
2025-09-12 00:15:42 +0000

    hello

--- diaffrin_api/admin.py
+++ diaffrin_api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from diaffrin_api.models import Commune, Entity
+from diaffrin_api.models import Commune, Entity,Personnel
 
 admin.site.register(Commune)
 admin.site.register(Entity)
+admin.site.register(Personnel)
