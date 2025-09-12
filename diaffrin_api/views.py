5edb72569b5246b38eba2db8dc5d870b22780d2a
absidibe <gouandiaka@gmail.com>
2025-09-12 00:31:20 +0000

    hello

--- diaffrin_api/views.py
+++ diaffrin_api/views.py
@@ -9,6 +9,8 @@ from diaffrin import settings
 from diaffrin_api.models import Commune, Entity,Personnel
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from django.http import HttpResponse
+
 
 
 
