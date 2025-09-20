897fc8a320e7c88349f2b3b3d68e481d8aadff67
absidibe <gouandiaka@gmail.com>
2025-09-20 23:40:16 +0200

    helpaiementlo

--- diaffrin_api/urls.py
+++ diaffrin_api/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path("api/paiement/list/", views.entity_paiements, name="paiement-list"),
     path('api/mobileauth/', views.login_view, name="mobileauth"),
     path('api/entity/create/', EntityBulkCreateView.as_view(), name='entity-create'),
-    path('api/entity/list/', EntityListView.as_view(), name='entity-list'),
+    path('api/entity/list/', EntityBulkCreateView.as_view(), name='entity-list'),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     re_path(r'^favicon\.ico$', favicon_view),
